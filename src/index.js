662564483d547cf03fb36ed57f624ebcdffba790
5idneyD <sldriscoll@btinternet.com>
2023-03-21 22:01:58 +0000

    Slow rendering when devtools open

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 
 function App() {
   return (
     <div>
       <h2>This is a flask and react boilerplate</h2>
+      <h2>We use esbuild as a bundler as it is super fast</h2>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
